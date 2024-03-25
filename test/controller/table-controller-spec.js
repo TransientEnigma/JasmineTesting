6854d2f0f580ebeb08a3bb611791d3d8ad2a2e68
TransientEnigma <e-engineer@hotmail.co.uk>
2024-03-25 18:15:22 +0000

    tidy up

--- test/controller/table-controller-spec.js
+++ test/controller/table-controller-spec.js
@@ -10,7 +10,11 @@ describe('TableController', function () {
         inject(function ($injector){
             $rootScope = $injector.get('$rootScope');
             $scope = $rootScope.$new();
-            dataServiceSpy = spyOnAngularService($injector.get('DataService'), 'getRemoteData', '{"data":[{"name":"Apple Pie","price":150},{"name":"Raspberry Pie","price":100},{"name":"Strawberry Pie","price":500000}]}');
+            dataServiceSpy = spyOnAngularService(
+                $injector.get('DataService'),
+                'getRemoteData',
+                '{"data":[{"name":"Apple Pie","price":150},{"name":"Raspberry Pie","price":100},{"name":"Strawberry Pie","price":500000}]}'
+            );
             controller = $injector.get('$controller')('TableController', {$scope: $scope});
         });
     });
@@ -26,7 +30,9 @@ describe('TableController', function () {
         it("should set the data to the result of the service call", function () {
             $scope.setTableData();
             expect(dataServiceSpy).toHaveBeenCalledTimes(1);
-            expect($scope.tableData).toEqual('{"data":[{"name":"Apple Pie","price":150},{"name":"Raspberry Pie","price":100},{"name":"Strawberry Pie","price":500000}]}');
+            expect($scope.tableData).toEqual(
+                '{"data":[{"name":"Apple Pie","price":150},{"name":"Raspberry Pie","price":100},{"name":"Strawberry Pie","price":500000}]}'
+            );
         });
     });
 
