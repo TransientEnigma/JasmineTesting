78a9c04d5ca42aa8e8d487bd303c4b7ee524f940
TransientEnigma <e-engineer@hotmail.co.uk>
2024-03-27 16:33:42 +0000

    tidy up and refactor

--- test/directives/text-and-reveal-spec.js
+++ test/directives/text-and-reveal-spec.js
@@ -11,7 +11,6 @@ describe("textAndReveal", function () {
     beforeEach(function () {
         // import directives
         module('directives');
-
         // use Angular inject to get rootScope (to create scope) and compile (to compile html)
         inject(function($injector){
             // grab rootScope
@@ -38,7 +37,6 @@ describe("textAndReveal", function () {
             $compile = $injector.get('$compile');
             // compile with scope, so we have the angular html element and scope
             textRevealHtmlElement = $compile(angular.element(textRevealElement))($scope)
-
         });
         // the HTML element is then added to our body
         $body.append(textRevealHtmlElement);
@@ -46,7 +44,7 @@ describe("textAndReveal", function () {
         // kick off the angular life cycle
         $rootScope.$digest();
 
-        // Grabs from the body. If it is there it confirms element has been compiled
+        // Grabs from directive in the body. If it is there it confirms element has been compiled
         $element = $('text-and-reveal');
     });
 
@@ -55,45 +53,36 @@ describe("textAndReveal", function () {
     })
 
     it("should render the directive in the DOM", function () {
-        expect($element.length).toEqual(1);
-    });
-
-    it("should render out the text from the scope", function () {
         // set the scopeText
         $scope.scopeText = "Sample Text";
-        // fire all the watches, so the scope expression {{something}} will be evaluated
+        // fire all the watchers, so the scope expression {{something}} will be evaluated
         $rootScope.$digest();
         // console.log('$element:', $element.length);
         expect($element.length).toEqual(1);
     });
 
-    it("should render out the text from the scope", function () {
+    it("should render out the h3 with text from the scope", function () {
         // set the scopeText
         $scope.scopeText = "Sample Text";
-        // fire all the watches, so the scope expression {{something}} will be evaluated
+        // fire all the watchers, so the scope expression {{something}} will be evaluated
         $rootScope.$digest();
-        // console.log('$element:', $element.attr('text'));
+        // confirm directive text attribute is set
         expect($element.attr('text')).toEqual("Sample Text");
+        // confirm the directive template renders with H3 text renders
+        expect($body.find('h3')[0].innerHTML).toEqual("Sample Text");
     });
 
-    it("it should render out the reveal from the scope", function () {
-        // set the scopeText
-        $scope.scopeReveal= "Yes";
-        // fire all the watches, so the scope expression {{something}} will be evaluated
-        $rootScope.$digest();
-        // console.log('$element:', $element.attr('reveal'));
-        expect($element.attr('reveal')).toEqual("Yes");
-    });
-
-    it("it should reveal the H5 with inner text when the scopeReveal is set", function () {
+    it("should render out the h5 with text from the scope", function () {
         $scope.scopeReveal= "Sample Text";
+        // fire all the watchers, so the scope expression {{something}} will be evaluated
         $scope.$digest();
-        // check we have the h5
-        // console.log($body.find('h5')[0]);
+        // confirm directive reveal attribute has text set
+        expect($element.attr('reveal')).toEqual("Sample Text");
+        // confirm the directive template renders with H5 text renders
         expect($body.find('h5')[0].innerHTML).toEqual("Sample Text");
     });
 
-    it("it should hide the h5 when the scopeReveal is not set", function () {
+    it("it should hide the h5 when the reveal attribute is not set", function () {
         $scope.scopeReveal= "";
         $scope.$digest();
         // check we have the class ng-hide
